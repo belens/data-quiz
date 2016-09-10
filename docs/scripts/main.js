4bb14763f6a416e0be48582b747d68dc352c527c
belens <sander.belen@gmail.com>
2016-09-10 22:15:27 +0200

    dist to docs for github pages

---
+++ docs/scripts/main.js
@@ -0,0 +1,1 @@
+"use strict";document.getElementById("one").addEventListener("click",function(){document.getElementById("shell").classList="anwsered",document.getElementById("topbox").innerHTML="asdf"});
