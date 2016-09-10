ac3483427e287a82dd20becd2493aa2be2396c17
belens <sander.belen@gmail.com>
2016-09-10 22:10:02 +0200

    initial push

---
+++ app/scripts/main.js
@@ -0,0 +1,4 @@
+document.getElementById("one").addEventListener("click", function(){
+    document.getElementById("shell").classList = "anwsered";
+    document.getElementById("topbox").innerHTML = 'asdf';
+});
